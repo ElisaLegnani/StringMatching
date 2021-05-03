71556889895b8d8a15ad7454123eb936d6193d88
Elisa Legnani <elegnani97@gmail.com>
2021-05-03 17:47:57 +0200

    Add algorithms tests

---
+++ test/algos.cpp
@@ -0,0 +1,43 @@
+#include "algos.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+  //AGGIUNGERE test per: calc_nextstate, transition, prefix
+
+  const char* text = "abacaa cba";
+  const char* pattern = "ba";
+  int shifts[10];
+  
+  // naive
+
+  naive(text, pattern, shifts);
+
+  if(shifts[0]!=0 || shifts[1]!=7){
+    cout << "Error: naive algorithm not working properly." << endl;
+    abort();
+  }
+
+  // finite_automaton
+
+  finite_automaton(text, pattern, shifts);
+
+  if(shifts[0]!=0 || shifts[1]!=7){
+    cout << "Error: finite_automaton algorithm not working properly." << endl;
+    abort();
+  }
+
+  // KMP
+
+  kmp(text, pattern, shifts);
+
+  if(shifts[0]!=0 || shifts[1]!=7){
+    cout << "Error: kmp algorithm not working properly." << endl;
+    abort();
+  }
+
+  return 0;
+}
