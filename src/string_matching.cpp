64f50b430d1eafb7d90c89c92fac5a233b7efcfe
Elisa Legnani <elegnani97@gmail.com>
2021-05-03 17:50:36 +0200

    Add time measure and shifts storage

--- src/string_matching.cpp
+++ src/string_matching.cpp
@@ -6,11 +6,23 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
+  //MODIFICARE - idea:
+  //(1) file di input con: - testo
+  //                       - pattern
+  //                       - algoritmo
+  //(2) file di output testo_pattern_algoritmo con: - shifts
+  //                                                - numero shifts
+  //                                                - tempo
+  //(3) funzioni compatte per: - lettura file input
+  //                           - lettura file testo
+  //                           - stampa su file output
+
   string filename_in; //ATTENZIONE: inserire files di testo senza caratteri strani - risolvere
   string p; // c'è modo più conveniente di leggere da file? senza convertire string --> char*
   string t;
   const char* pattern;
   const char* text;
+  int shifts[100]; //c'è modo di non fissarlo?
 
   if (argv[1] == NULL) {
     cout << "Insert input filename: ";
@@ -28,16 +40,58 @@ int main(int argc, char *argv[]) {
   t = string((istreambuf_iterator<char>(stream)), istreambuf_iterator<char>());
   text = t.c_str();
 
-  cout << "NAIVE:" << endl;
-  naive(text, pattern);
+  cout << endl << "NAIVE:" << endl;
+
+  clock_t ti = clock();
+  naive(text, pattern, shifts);
+  clock_t tf = clock();
+  double dt = double(tf-ti)*1000 /CLOCKS_PER_SEC;
+
+  int i{};
+  cout << "P found with shifts: ";
+  do{
+    cout << shifts[i] << "  ";
+    i++;
+  }while(shifts[i]!=0);
+
+  cout << endl << "time: " << dt << endl;
+  cout << "P found " << i << " times" << endl;
 
   cout << endl << "FINITE AUTOMATON:" << endl;
-  finite_automaton(text, pattern);
+
+  ti = clock();
+  finite_automaton(text, pattern, shifts);
+  tf = clock();
+  dt = double(tf-ti)*1000 /CLOCKS_PER_SEC;
+
+  i=0;
+  cout << "P found with shifts: ";
+  do{
+    cout << shifts[i] << "  ";
+    i++;
+  }while(shifts[i]!=0);
+
+  cout << endl << "time: " << dt << endl;
+  cout << "P found " << i << " times" << endl;
+
 
   cout << endl << "KMP:" << endl;
-  kmp(text, pattern);
 
-  // stampare tempi
+  ti = clock();
+  kmp(text, pattern, shifts);
+  tf = clock();
+  dt = double(tf-ti)*1000 /CLOCKS_PER_SEC;
+
+  i=0;
+  cout << "P found with shifts: ";
+  do{
+    cout << shifts[i] << "  ";
+    i++;
+  }while(shifts[i]!=0);
+
+  cout << endl << "time: " << dt << endl;
+  cout << "P found " << i << " times" << endl;
+
 
   return 0;
 }
